e39ef4779c140cfc0a8774d65144248c265c83d2
Dani Bodor <d.bodor@esciencecenter.nl>
2023-06-30 02:58:43 +0200

    run cookiecutter

---
+++ dash/dash/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about dash"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Dani Bodor"
+__email__ = "d.bodor@esciencecenter.nl"
+__version__ = "0.1.0"
