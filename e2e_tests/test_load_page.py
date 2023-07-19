7d04b376bba9d331d7274cdbf791093c3d8c5509
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2023-07-19 17:21:05 +0200

    linitng and better naming

--- e2e_tests/test_load_page.py
+++ e2e_tests/test_load_page.py
@@ -1,24 +1,20 @@
 import eit_dash.definitions.element_ids as ids
-import dash
-from dash import html
+
 from eit_dash.main import app
 
 
-# 2. give each testcase a test case ID, and pass the fixture
-# dash_duo as a function argument
-def test_001_child_with_0(dash_duo):
-    # 3. define your app inside the test function
-    # 4. host the app locally in a thread, all dash server configs could be
-    # passed after the first app argument
+def test_001_load_page_load_data(dash_duo):
+    # start the app
     dash_duo.start_server(app)
-    # 5. use wait_for_* if your target element is the result of a callback,
-    # keep in mind even the initial rendering can trigger callbacks
+    # go to the loading page
     dash_duo.wait_for_page(dash_duo.server_url + '/load', timeout=5)
-    # 6. use this form if its present is expected at the action point
+    # click the add data button
     dash_duo.multiple_click(f'#{ids.ADD_DATA_BUTTON}', clicks=1)
+    # click the select button
     dash_duo.multiple_click(f'#{ids.SELECT_FILES_BUTTON}', clicks=1)
+    # click the confirm button
     dash_duo.multiple_click(f'#{ids.LOAD_CONFIRM_BUTTON}', clicks=1)
-    dash_duo.wait_for_element_by_id('card-1')
+    # wait for the new card to be added
+    dash_duo.wait_for_element_by_id('card-1', timeout=10)
+    # assert that the card exists
     assert dash_duo.find_element(f'#{ids.DATASET_CONTAINER}').text != ''
-    # 8. visual testing with percy snapshot
-    dash_duo.percy_snapshot("test_001_child_with_0-layout")
