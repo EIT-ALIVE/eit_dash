451bdcc0f24e0bda92988a9a3acbdcabf5cc37d9
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2023-07-19 13:48:01 +0200

    actual tests

---
+++ tests/test_load_callbacks.py
@@ -0,0 +1,43 @@
+import dash_bootstrap_components as dbc
+import eit_dash.definitions.element_ids as ids
+
+from dash._callback_context import context_value
+from dash._utils import AttributeDict
+from dash import html
+from eit_dash.callbacks.load_callbacks import open_modal, load_file, show_info
+
+
+def test_show_info_callback():
+    output = show_info(1, None, 0)
+
+    mock_data_card = [dbc.Card(dbc.CardBody([
+        html.H4("Dataset 1", className="card-title"),
+        html.H6("Timpel", className="card-subtitle"),
+        dbc.Row("N_signals: 3", style={'margin-left': 10}),
+        dbc.Row("duration: 12", style={'margin-left': 10}),
+        dbc.Row("n_frames: 234", style={'margin-left': 10}),
+        dbc.Row("filename: file.bin", style={'margin-left': 10}),
+        dbc.Row("etc: etc", style={'margin-left': 10})
+    ]))]
+
+    # Assessing the string converted objects, to check that the properties are the same.
+    # Assessing for the equivalence of the objects directly will fail
+    assert str(output) == str(mock_data_card)
+
+
+def test_load_file_callback():
+    output = load_file(True)
+    assert output is False
+
+
+def test_open_modal_callback():
+
+    context_value.set(AttributeDict(**{"triggered_inputs": [
+        {"prop_id": f"{ids.SELECT_FILES_BUTTON}.n_clicks"}
+    ]}))
+
+    output = open_modal(1, 1)
+
+    expected_output = (True, True)
+
+    assert output == expected_output
