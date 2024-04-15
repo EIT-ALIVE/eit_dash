c0e05057c840867509641b1243fbed3f9123154a
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2024-04-15 15:07:44 +0200

    create analyze page

--- eit_dash/main.py
+++ eit_dash/main.py
@@ -2,16 +2,22 @@ import dash_bootstrap_components as dbc
 from dash import html, page_container
 
 from eit_dash.app import app
-from eit_dash.callbacks import load_callbacks, preprocessing_callbacks  # noqa: F401
+from eit_dash.callbacks import (
+    load_callbacks,
+    preprocessing_callbacks,
+    analyze_callbacks,
+)  # noqa: F401
 
 app.layout = html.Div(
     [
-        html.H1(id="test-id", children="EIT-ALIVE dashboard", style={"textAlign": "center"}),
+        html.H1(
+            id="test-id", children="EIT-ALIVE dashboard", style={"textAlign": "center"}
+        ),
         dbc.Row(
             [
                 dbc.Col(dbc.NavLink("Load", href="/load")),
                 dbc.Col(dbc.NavLink("Pre-processing", href="/preprocessing")),
-                dbc.Col(dbc.NavLink("Analyze", href="/dummy")),
+                dbc.Col(dbc.NavLink("Analyze", href="/analyze")),
                 dbc.Col(dbc.NavLink("Summarize", href="/dummy")),
             ],
             style={"textAlign": "center"},
