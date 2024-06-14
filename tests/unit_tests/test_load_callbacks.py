1ce187c5ab3d46e67d0e42e7aa643c478d9aaadb
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2024-06-14 15:42:58 +0200

    added conftest file

--- tests/unit_tests/test_load_callbacks.py
+++ tests/unit_tests/test_load_callbacks.py
@@ -1,33 +1,19 @@
-from pathlib import Path
 from unittest.mock import patch
 
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
+import pytest
 from dash import html
 from dash._callback_context import context_value
 from dash._utils import AttributeDict
 from eitprocessing.datahandling.eitdata import Vendor
-from eitprocessing.datahandling.loading import load_eit_data
+from eitprocessing.datahandling.sequence import Sequence
 
 import eit_dash.definitions.element_ids as ids
 import eit_dash.definitions.layout_styles as styles
 from eit_dash.callbacks.load_callbacks import load_selected_data, show_info
 from eit_dash.definitions.option_lists import InputFiletypes
-
-data_path = Path(__file__).resolve().parent.parent / "test_data" / "Draeger_Test_4.bin"
-
-file_data = load_eit_data(
-    data_path,
-    vendor="draeger",
-    label="selected data",
-)
-
-test_file_length = len(file_data.time)
-
-first_sample = 1000
-last_sample = 2000
-
-cut_file_length = last_sample - first_sample
+from tests.conftest import data_path
 
 expected_continuous_data = [
     {"label": "airway pressure", "value": 0},
@@ -36,24 +22,32 @@ expected_continuous_data = [
     {"label": "CO2", "value": 3},
 ]
 
-expected_cut_info_data = {
-    "Name": "Draeger_Test_4.bin",
-    "n_frames": cut_file_length,
-    "start_time": file_data.time[first_sample],
-    "end_time": file_data.time[last_sample - 1],
-    "vendor": Vendor.DRAEGER,
-    "continuous signals": [
-        "airway pressure",
-        "flow",
-        "volume",
-        "CO2",
-        "global_impedance_(raw)",
-    ],
-    "path": str(data_path),
-}
+first_sample = 100
+last_sample = 1000
+
+cut_file_length = last_sample - first_sample
 
 
-def test_load_selected_data_callback():
+@pytest.fixture(scope="session")
+def expected_cut_info_data(file_data: Sequence):
+    return {
+        "Name": "Draeger_Test3.bin",
+        "n_frames": cut_file_length,
+        "start_time": file_data.time[first_sample],
+        "end_time": file_data.time[last_sample - 1],
+        "vendor": Vendor.DRAEGER,
+        "continuous signals": [
+            "airway pressure",
+            "flow",
+            "volume",
+            "CO2",
+            "global_impedance_(raw)",
+        ],
+        "path": str(data_path),
+    }
+
+
+def test_load_selected_data_callback(file_data: Sequence, expected_cut_info_data: dict):
     cancel_load = 0
     sig = []
     file_type = InputFiletypes.Draeger.value
@@ -83,27 +77,30 @@ def test_load_selected_data_callback():
     # we can check the data in the figure to verify the correct data loading
     fig_data = output[3].data[0]["x"]
 
-    assert len(fig_data) == test_file_length
+    assert len(fig_data) == len(file_data.time)
 
     # we can check that also the other continuous data has been detected and displayed as options
 
     assert output[1] == expected_continuous_data
 
 
-@patch("eit_dash.callbacks.load_callbacks.file_data", new=file_data)
-def test_show_info_callback():
+def test_show_info_callback(file_data: Sequence, expected_cut_info_data: dict):
     # run the callback
-    output = show_info(
-        btn_click=1,
-        loaded_data=data_path,
-        container_state=None,
-        filetype="1",
-        slidebar_stat={
-            "xaxis.range": [file_data.time[first_sample], file_data.time[last_sample]],
-        },
-        selected_signals=[0, 1, 2, 3],
-        signals_options=expected_continuous_data,
-    )
+    with patch("eit_dash.callbacks.load_callbacks.file_data", new=file_data):
+        output = show_info(
+            btn_click=1,
+            loaded_data=data_path,
+            container_state=None,
+            filetype="1",
+            slidebar_stat={
+                "xaxis.range": [
+                    file_data.time[first_sample],
+                    file_data.time[last_sample],
+                ],
+            },
+            selected_signals=[0, 1, 2, 3],
+            signals_options=expected_continuous_data,
+        )
 
     # the output is a card containing the information about the selected signal.
     # We have to check that the information is the expected one to verify the correct slicing
--- tests/unit_tests/test_preprocessing_callbacks.py
+++ tests/unit_tests/test_preprocessing_callbacks.py
@@ -1,10 +1,9 @@
-from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 from dash._callback_context import context_value
 from dash._utils import AttributeDict
-from eitprocessing.datahandling.loading import load_eit_data
+from eitprocessing.datahandling.sequence import Sequence
 
 import eit_dash.definitions.element_ids as ids
 from eit_dash.callbacks.preprocessing_callbacks import (
@@ -16,58 +15,60 @@ from eit_dash.definitions.option_lists import FilterTypes
 from eit_dash.utils.data_singleton import LoadedData
 
 
-def prepare_data():
-    data_path = Path(__file__).resolve().parent.parent / "test_data" / "Draeger_Test_4.bin"
-    file_data = load_eit_data(
-        data_path,
-        vendor="draeger",
-        label="selected data",
-    )
-
+@pytest.fixture()
+def mock_data_object(file_data: Sequence):
     data_object = LoadedData()
     data_object.add_sequence(file_data)
     data_object.add_stable_period(file_data, 0, 0)
 
-    tmp_results = LoadedData()
-
-    return data_object, tmp_results
+    return data_object
 
 
-mock_data_object, mock_tmp_results = prepare_data()
+@pytest.fixture()
+def mock_tmp_results():
+    return LoadedData()
 
 
-@patch("eit_dash.callbacks.preprocessing_callbacks.data_object", new=mock_data_object)
-@patch("eit_dash.callbacks.preprocessing_callbacks.tmp_results", new=mock_tmp_results)
-def test_apply_filter_callback():
+def test_apply_filter_callback(
+    mock_data_object: LoadedData,
+    mock_tmp_results: LoadedData,
+):
     low_cut = 1
     high_cut = 9
     filter_order = 1
 
-    # test error in filter
-    with pytest.raises(TypeError):
+    with patch(
+        "eit_dash.callbacks.preprocessing_callbacks.data_object",
+        new=mock_data_object,
+    ), patch(
+        "eit_dash.callbacks.preprocessing_callbacks.tmp_results",
+        new=mock_tmp_results,
+    ):
+        # test error in filter
+        with pytest.raises(TypeError):
+            _ = apply_filter(
+                0,
+                co_low=low_cut,
+                co_high=high_cut,
+                order=filter_order,
+                filter_selected=FilterTypes.lowpass.value,
+                results=[],
+            )
+
+        # test valid filter
         _ = apply_filter(
             0,
             co_low=low_cut,
             co_high=high_cut,
             order=filter_order,
-            filter_selected=FilterTypes.lowpass.value,
+            filter_selected=FilterTypes.bandpass.value,
             results=[],
         )
 
-    # test valid filter
-    _ = apply_filter(
-        0,
-        co_low=low_cut,
-        co_high=high_cut,
-        order=filter_order,
-        filter_selected=FilterTypes.bandpass.value,
-        results=[],
-    )
-
-    # the filtered results are saved in a temporary object before saving them
-    # through a different call. We need to verify if the presence of the data
-    # in the mocked temporary object.
-    assert "global_impedance_(filtered)" in mock_tmp_results.get_stable_period(0).get_data().continuous_data
+        # the filtered results are saved in a temporary object before saving them
+        # through a different call. We need to verify if the presence of the data
+        # in the mocked temporary object.
+        assert "global_impedance_(filtered)" in mock_tmp_results.get_stable_period(0).get_data().continuous_data
 
 
 def test_open_synch_modal_callback():
@@ -99,7 +100,9 @@ def test_open_synch_modal_callback():
 def test_open_periods_modal_callback():
     context_value.set(
         AttributeDict(
-            triggered_inputs=[{"prop_id": f"{ids.OPEN_SELECT_PERIODS_BUTTON}.n_clicks"}],
+            triggered_inputs=[
+                {"prop_id": f"{ids.OPEN_SELECT_PERIODS_BUTTON}.n_clicks"},
+            ],
         ),
     )
 
