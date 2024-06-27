d976af909fb7f280087c22c8592c08c1c1a14150
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2024-06-27 14:09:51 +0200

    linting

--- cli/cli.py
+++ cli/cli.py
@@ -1,11 +1,12 @@
 import click
+
 from eit_dash.app import app
 
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
-    """Default prompt. It shows the help command"""
+    """Default prompt. It shows the help command."""
     if ctx.invoked_subcommand is None:
         click.echo(ctx.get_help())
 
@@ -13,7 +14,6 @@ def cli(ctx):
 @cli.command(name="run", help="Start the dashboard.")
 def run():
     """Start the dashboard."""
-
     app.run_server(debug=False)
 
 
